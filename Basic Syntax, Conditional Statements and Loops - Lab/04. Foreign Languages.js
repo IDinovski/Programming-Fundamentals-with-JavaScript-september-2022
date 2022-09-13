29a3be33b555deebdd99a6287505b414cefd263a
IDinovski <idinovski@gmail.com>
2022-09-13 05:08:46 +0300

    Create 04. Foreign Languages.js

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/04. Foreign Languages.js
@@ -0,0 +1,11 @@
+function solve(language){
+if(language=="USA" || language=="England"){
+    console.log("English");
+}else if(language=="Spain"||language=="Argentina"||language=="Mexico")
+{
+    console.log("Spanish");
+}else{
+    console.log("unknown");
+}
+}
+solve (["d"])
