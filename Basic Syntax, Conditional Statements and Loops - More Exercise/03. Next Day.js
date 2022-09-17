992cb39dafc19a568de0b269d7174bb06f801eba
IDinovski <idinovski@gmail.com>
2022-09-17 05:56:54 +0300

    Create 03. Next Day.js

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/03. Next Day.js
@@ -0,0 +1,8 @@
+function solve(year, month, day) {
+  let nextDay = new Date(year, month - 1, day + 1);
+  let newYear = nextDay.getFullYear();
+  let newMonth = nextDay.getMonth() + 1;
+  let newDate = nextDay.getDate();
+  console.log(`${newYear}-${newMonth}-${newDate}`);
+}
+solve(2022, 09, 0);
