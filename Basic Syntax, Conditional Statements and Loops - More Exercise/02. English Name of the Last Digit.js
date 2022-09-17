06f3a84a070be4dd129810629ebea37e3c17f87f
IDinovski <idinovski@gmail.com>
2022-09-17 05:56:33 +0300

    Create 02. English Name of the Last Digit.js

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/02. English Name of the Last Digit.js
@@ -0,0 +1,36 @@
+function solve(m) {
+  let lastDigit = String(m).slice(-1);
+  switch (lastDigit) {
+    case "0":
+      console.log("zero");
+      break;
+    case "1":
+      console.log("one");
+      break;
+    case "2":
+      console.log("two");
+      break;
+    case "3":
+      console.log("three");
+      break;
+    case "4":
+      console.log("four");
+      break;
+    case "5":
+      console.log("five");
+      break;
+    case "6":
+      console.log("six");
+      break;
+    case "7":
+      console.log("seven");
+      break;
+    case "8":
+      console.log("eight");
+      break;
+    case "9":
+      console.log("nine");
+      break;
+  }
+}
+solve(5121);
