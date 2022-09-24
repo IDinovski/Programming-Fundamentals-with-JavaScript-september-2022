c9ccf465970611dd06c954ed0322aadca1edc6f1
IDinovski <idinovski@gmail.com>
2022-09-24 16:46:40 +0300

    Create 01. Digits with Words.js

---
+++ Data Types and Variables - More Exercise/01. Digits with Words.js
@@ -0,0 +1,35 @@
+function solve(str) {
+  switch (str) {
+    case "zero":
+      str = 0;
+      break;
+    case "one":
+      str = 1;
+      break;
+    case "two":
+      str = 2;
+      break;
+    case "three":
+      str = 3;
+      break;
+    case "four":
+      str = 4;
+      break;
+    case "five":
+      str = 5;
+      break;
+    case "six":
+      str = 6;
+      break;
+    case "seven":
+      str = 7;
+      break;
+    case "eight":
+      str = 8;
+      break;
+    case "nine":
+      str = 9;
+      break;
+  }
+  console.log(str);
+}
