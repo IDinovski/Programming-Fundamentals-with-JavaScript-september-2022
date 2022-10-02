8e449f58a9ff0dadc67496dfc3950d1c38917cdc
IDinovski <idinovski@gmail.com>
2022-10-02 11:20:58 +0300

    Create 07. Spiral Matrix.js

---
+++ Arrays - More Exercise/07. Spiral Matrix.js
@@ -0,0 +1,20 @@
+function solve(num1, num2) {
+  let max = num1 * num2;
+  let result = [];
+  let top = 1;
+  let right = 1;
+  let botton = 1;
+  let left = 1;
+
+  for (let i = 1; i <= max; i++) {
+    if(top<=5){
+        result.push[i]
+        top++
+    }else if()
+    if (result.length >= 5) {
+      console.log(result.join(" "));
+      result=[]
+    }
+  }
+}
+solve(5, 5);
