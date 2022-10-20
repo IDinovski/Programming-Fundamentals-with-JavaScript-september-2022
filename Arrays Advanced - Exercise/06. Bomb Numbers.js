d3f8a48d654588a6ce4d8e075504175f189e28e7
IDinovski <idinovski@gmail.com>
2022-10-20 10:04:53 +0300

    Update 06. Bomb Numbers.js

--- Arrays Advanced - Exercise/06. Bomb Numbers.js
+++ Arrays Advanced - Exercise/06. Bomb Numbers.js
@@ -1,10 +1,17 @@
 function bombNumbers(sequence, bomb) {
-  let specialNumber = bomb[0];
-  let bombPower = bomb[1];
-
-  while (sequence.includes(specialNumber)) {
-    let index = sequence.indexOf(specialNumber);
-    let elementsToRemove = bombPower * 2 + 1;
-    let startIndex = index - bombPower;
-
-   
+    let specialNumber = bomb[0];
+    let bombPower = bomb[1];
+ 
+    while (sequence.includes(specialNumber)) {
+        let index = sequence.indexOf(specialNumber);
+        let elementsToRemove = bombPower * 2 + 1;
+        let startIndex = index - bombPower;
+ 
+        if (startIndex < 0) {
+            elementsToRemove += startIndex;
+            startIndex = 0;
+        }
+        sequence.splice(startIndex, elementsToRemove);
+    }
+    console.log(sequence.reduce((a, b) => a + b, 0));
+}
