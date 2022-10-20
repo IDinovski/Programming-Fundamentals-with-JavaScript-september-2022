f8abaee08482c1777373995858cf8b04c568c73f
IDinovski <idinovski@gmail.com>
2022-10-20 09:17:47 +0300

    Update 04. Sorting.js

--- Arrays Advanced - Exercise/04. Sorting.js
+++ Arrays Advanced - Exercise/04. Sorting.js
@@ -1,4 +1,14 @@
-function solve(arr){
-
+function solve(arr) {
+  arr.sort((a, b) => a - b);
+  let length = arr.length;
+  let newArr = [];
+  for (let i = 0; i < length; i++) {
+    if (i % 2 === 0) {
+      newArr.push(arr.pop());
+    } else if (i % 2 !== 0) {
+      newArr.push(arr.shift());
+    }
+  }
+  console.log(newArr.join(" "));
 }
-solve([1, 21, 3, 52, 69, 63, 31, 2, 18, 94])
+solve([1, 21, 3, 52, 69, 63, 31, 2, 18, 94]);
