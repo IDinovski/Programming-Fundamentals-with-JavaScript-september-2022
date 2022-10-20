45cfc0ff3cff35cb2fb8babc16bbd923512d64d1
IDinovski <idinovski@gmail.com>
2022-10-20 10:30:15 +0300

    Update 07. Search for a Number.js

---
+++ Arrays Advanced - Exercise/07. Search for a Number.js
@@ -0,0 +1,7 @@
+function solve(arr, conditions) {
+  let numberOfElementsToTake = conditions[0];
+  let numberOfElementsToDelete = conditions[1];
+  let specialNumber = conditions[2];
+  
+}
+solve([5, 2, 3, 4, 1, 6], [5, 2, 3]);
