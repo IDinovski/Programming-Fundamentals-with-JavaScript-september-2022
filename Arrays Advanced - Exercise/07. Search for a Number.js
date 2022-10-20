5433013c6605e5c3c5e39089a763b5cba11fc020
IDinovski <idinovski@gmail.com>
2022-10-20 10:38:44 +0300

    Update 07. Search for a Number.js

--- Arrays Advanced - Exercise/07. Search for a Number.js
+++ Arrays Advanced - Exercise/07. Search for a Number.js
@@ -2,6 +2,16 @@ function solve(arr, conditions) {
   let numberOfElementsToTake = conditions[0];
   let numberOfElementsToDelete = conditions[1];
   let specialNumber = conditions[2];
-  
+  let newArr = arr
+    .slice(0, numberOfElementsToTake)
+    .slice(numberOfElementsToDelete, arr.length);
+  let counter = 0;
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] === specialNumber) {
+      counter++;
+    }
+  }
+
+  console.log(`Number ${specialNumber} occurs ${counter} times.`);
 }
 solve([5, 2, 3, 4, 1, 6], [5, 2, 3]);
