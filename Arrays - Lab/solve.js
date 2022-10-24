50198b062c8ce8d5b05b0701b1c2deca250e337f
IDinovski <idinovski@gmail.com>
2022-10-24 21:40:06 +0300

    Update solve.js

--- Arrays - Lab/solve.js
+++ Arrays - Lab/solve.js
@@ -1,19 +1,7 @@
-function solve() {
-  let days = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
-  let a;
-  if (days.includes("Friday")) {
-    a = days.indexOf("Friday");
-    
-    days[a] = "proba"
-  }
-  console.log(days.indexOf("Monday"))
+function solve(num, arr) {
+  arr.splice(0, 3)
+  
+
+  console.log(arr);
 }
-solve();
+solve(3, [10, 20, 30, 40, 50]);
