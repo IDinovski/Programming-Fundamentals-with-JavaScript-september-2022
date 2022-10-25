73e4246b9e7f324beee97dfc324519ffb115f3b8
IDinovski <idinovski@gmail.com>
2022-10-25 07:06:04 +0300

    Update solve.js

--- Arrays - Exercise/solve.js
+++ Arrays - Exercise/solve.js
@@ -1,8 +1,21 @@
-function solve(arr, rotations) {
-  for (let i = 0; i < rotations; i++) {
-    let char = arr.shift();
-    arr.push(char);
+function solve(arr) {
+  let flag = true;
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let k = i + 1; k < arr.length; k++) {
+      if (arr[i] > arr[k]) {
+        flag = true;
+      } else {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      result.push(arr[i]);
+    }
+    flag = false;
   }
-  console.log(arr.join(" "));
+  result.push(arr[arr.length-1])
+  console.log(result.join(" "));
 }
-solve([51, 47, 32, 61, 21], 2);
+solve([1, 4, 5, 3, 2]);
