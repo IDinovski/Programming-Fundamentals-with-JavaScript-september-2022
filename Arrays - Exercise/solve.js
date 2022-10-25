93106fda40635ceebcbd24fdc6005fb03aa5ec76
IDinovski <idinovski@gmail.com>
2022-10-25 08:48:12 +0300

    Update solve.js

--- Arrays - Exercise/solve.js
+++ Arrays - Exercise/solve.js
@@ -1,24 +1,4 @@
-function solve(arr) {
-  let leftSum = 0;
-  let rightSum = 0;
-  let flag = true;
-  for (let i = 0; i < arr.length; i++) {
-    let currentNumber = arr[i];
-    for (let l = i - 1; l >= 0; l--) {
-      leftSum += arr[l];
-    }
-    for (let r = i + 1; r < arr.length; r++) {
-      rightSum += arr[r];
-    }
-    if (leftSum == rightSum) {
-      console.log(i);
-      flag = false;
-    }
-    leftSum = 0;
-    rightSum = 0;
-  }
-  if (flag) {
-    console.log("no");
-  }
-}
-solve([1]);
+const months = ["Jan", "March", "April", "June"];
+let check = months.splice(1, 0, 'feb');
+console.log(check);
+console.log(months);
