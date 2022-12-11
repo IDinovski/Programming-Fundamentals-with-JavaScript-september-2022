d6fd9382962c99cef023ee237593408b7c1394ba
IDinovski <idinovski@gmail.com>
2022-12-11 14:59:34 +0200

    Create 04. Convert to JSON.js

---
+++ Objects and Classes - Lab/04. Convert to JSON.js
@@ -0,0 +1,11 @@
+function convertToJSON(firstName,lastName,color){
+let obj = {
+    firstName,
+    lastName,
+    color
+};
+let result = JSON.stringify(obj)
+console.log(result);
+
+}
+convertToJSON('George', 'Jones', 'Brown')
