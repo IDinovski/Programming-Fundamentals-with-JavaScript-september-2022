1701635fd8e64914a6d5e848d0257be949189e32
IDinovski <idinovski@gmail.com>
2022-12-12 18:20:41 +0200

    Update 01. Employees.js

--- Objects and Classes - Exercise/01. Employees.js
+++ Objects and Classes - Exercise/01. Employees.js
@@ -1,10 +1,18 @@
-function solve(input){
-    
+function solve(input) {
+  let listObject = {};
+
+  let phones = [];
+  for (let name of input) {
+    listObject.name = name;
+    listObject.number = name.length;
+    console.log(
+      `Name: ${listObject.name} -- Personal Number: ${listObject.number}`
+    );
+  }
 }
 solve([
-'Silas Butler',
-'Adnaan Buckley',
-'Juan Peterson',
-'Brendan Villarreal'
-]
-)
+  "Silas Butler",
+  "Adnaan Buckley",
+  "Juan Peterson",
+  "Brendan Villarreal",
+]);
